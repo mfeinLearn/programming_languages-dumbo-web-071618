c89201bf9ada6ee97b4ffa6969290e7920103846
Malcome Fein <mfein90@gmail.com>
2018-08-03 06:27:33 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,8 +2,7 @@ def reformat_languages(languages)
   # your code here
   new_hash = {}
     languages.each do |key, array|
-    puts "#{key}-----"
-    puts array
+      binding.pry
   end
   new_hash
 end
